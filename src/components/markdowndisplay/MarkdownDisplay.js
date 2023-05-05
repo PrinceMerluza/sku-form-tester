d95baedc669ee3fac25e68968e4c1fd18597e805
PrinceMerluza <zero.bash@Gmail.com>
2023-05-05 22:54:21 +0800

    2 files modified, 1 added, 2 deleted

--- src/components/markdowndisplay/MarkdownDisplay.js
+++ src/components/markdowndisplay/MarkdownDisplay.js
@@ -3,21 +3,35 @@ import { default as ReactMarkdown } from 'react-markdown/with-html';
 
 import { default as Renderers } from '../../unifiedplugins/Renderers';
 import { AlertBlock } from '../../unifiedplugins/AlertBlock';
-import { Headings } from '../../unifiedplugins/Headings';
 import { Paragraph } from '../../unifiedplugins/Paragraph';
-import { TableOfContents } from '../../unifiedplugins/TableOfContents';
 import { CodeFence } from '../../unifiedplugins/CodeFence';
 import { Table } from '../../unifiedplugins/Table';
 import { Image } from '../../unifiedplugins/Image';
 import { List } from '../../unifiedplugins/List';
 import { TabbedContent } from '../../unifiedplugins/TabbedContent';
 import { DxUiComponent } from '../../unifiedplugins/DxUiComponent';
-import { HeadingTransformer } from '../../unifiedplugins/HeadingTransformer';
 
 function MarkdownDisplay(props) {
-	const plugins = [AlertBlock, Headings, Paragraph, TableOfContents, CodeFence, Table, Image, List, TabbedContent, DxUiComponent];
-	const astPlugins = [HeadingTransformer];
-	return <ReactMarkdown source={props.markdown} escapeHtml={false} plugins={plugins} renderers={Renderers} astPlugins={astPlugins} />;
+  const plugins = [
+    AlertBlock,
+    Paragraph,
+    CodeFence,
+    Table,
+    Image,
+    List,
+    TabbedContent,
+    DxUiComponent,
+  ];
+  const astPlugins = [HeadingTransformer];
+  return (
+    <ReactMarkdown
+      source={props.markdown}
+      escapeHtml={false}
+      plugins={plugins}
+      renderers={Renderers}
+      astPlugins={astPlugins}
+    />
+  );
 }
 
 export default MarkdownDisplay;
