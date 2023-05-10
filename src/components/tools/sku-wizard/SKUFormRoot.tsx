7e351b2390ed44e08959c6575170069b727494c3
Prince Merluza <zero.bash@gmail.com>
2023-05-10 20:54:29 +0800

    minor change

--- src/components/tools/sku-wizard/SKUFormRoot.tsx
+++ src/components/tools/sku-wizard/SKUFormRoot.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { DxButton, DxTextbox, DxItemGroup, DxItemGroupItem } from 'genesys-react-components';
 import { UsageProduct, MeteredProduct, FlatFeeProduct, OneTimeProduct, EmptyProduct } from './types';
 import SKUForm from './SKUForm';
+import ValidationFieldContainer from '../utils/validation/ValidationFieldContainer';
+import Validator from '../utils/validation/Validator';
 
 import './SKUFormRoot.scss';
 import { forEach } from 'jszip';
@@ -35,7 +37,26 @@ export default function SKUFormRoot() {
 	const [formsSavedStatus, setFormsSavedStatus] = useState<{ [key: string]: boolean }>({}); // Tracks if SKU forms are saved
 	const [addProductEnabled, setAddProductEnabled] = useState<boolean>(true);
 	const [currency, setCurrency] = useState<string>('USD');
+
+	// Contact Information page state
+	const [subNotificationEmail, setSubNotificationEmail] = useState<string>();
+	const [salesLeadEmail, setSalesLeadEmail] = useState<string>();
+	const [productTOS, setProductTOS] = useState<string>();
+	const [quoteNotes, setQuoteNotes] = useState<string>();
+	const [onContactsPage, setOnContactsPage] = useState<boolean>(false);
 	const [errors, setErrors] = useState<{ [key: string]: Array<string> }>({});
+
+	// Vadidation
+	useEffect(() => {
+		const newErrors: { [key: string]: Array<string> } = {};
+		const validator: Validator = new Validator(newErrors);
+
+		validator.validateEmail('subNotificationEmail', subNotificationEmail, false);
+		validator.validateEmail('salesLeadEmail', salesLeadEmail, false);
+		validator.validateURL('productTOS', productTOS, false);
+
+		setErrors(newErrors);
+	}, [subNotificationEmail, salesLeadEmail, productTOS, quoteNotes, currency]);
 
 	const setFormStatus = (id: string, val: boolean) => {
 		setFormsSavedStatus((oldStatus) => {
@@ -107,50 +128,105 @@ export default function SKUFormRoot() {
 		});
 	};
 
+	const getContactsForm = () => {
+		return (
+			<div className="required-container">
+				<h2>Contact Information</h2>
+				<ValidationFieldContainer errors={errors} name="subNotificationEmail">
+					<DxTextbox
+						inputType="text"
+						label="Subscription Notification Email"
+						placeholder="email@domain.com"
+						clearButton={true}
+						value={subNotificationEmail}
+						onChange={(newValue) => setSubNotificationEmail(newValue)}
+						changeDebounceMs={100}
+					/>
+				</ValidationFieldContainer>
+				<ValidationFieldContainer errors={errors} name="salesLeadEmail">
+					<DxTextbox
+						inputType="text"
+						label="Sales Lead Email"
+						placeholder="sales@domain.com"
+						clearButton={true}
+						value={salesLeadEmail}
+						onChange={(newValue) => setSalesLeadEmail(newValue)}
+						changeDebounceMs={100}
+					/>
+				</ValidationFieldContainer>
+				<ValidationFieldContainer errors={errors} name="productTOS">
+					<DxTextbox
+						inputType="text"
+						label="Link to Product Terms and Conditions"
+						placeholder="https://www.company.com/tos"
+						clearButton={true}
+						value={productTOS}
+						onChange={(newValue) => setProductTOS(newValue)}
+						changeDebounceMs={100}
+					/>
+				</ValidationFieldContainer>
+				<ValidationFieldContainer errors={errors} name="quoteNotes">
+					<DxTextbox
+						inputType="textarea"
+						label="How to Quote the Product"
+						placeholder="Details..."
+						clearButton={true}
+						value={quoteNotes}
+						onChange={(newValue) => setQuoteNotes(newValue)}
+						changeDebounceMs={100}
+					/>
+				</ValidationFieldContainer>
+			</div>
+		);
+	};
+
 	return (
 		<div className="sku-form-root">
-			<div>
-				<DxItemGroup
-					title="Currency"
-					items={currencyList}
-					format="dropdown"
-					onItemChanged={(item, isSelected) => {
-						if (isSelected) setCurrency(item.value);
-					}}
-				/>
-				<div>{`FxRate: ${currency}/USD = ${fxRate[currency]}`}</div>
+			<div className={`editing-page ${onContactsPage ? 'hidden' : ''}`}>
+				<div>
+					<DxItemGroup
+						title="Currency"
+						items={currencyList}
+						format="dropdown"
+						onItemChanged={(item, isSelected) => {
+							if (isSelected) setCurrency(item.value);
+						}}
+					/>
+					<div>{`FxRate: ${currency}/USD = ${fxRate[currency]}`}</div>
+				</div>
+				<div>{SKUList}</div>
+				<div className={`add-product ${addProductEnabled ? '' : 'hidden'}`}>
+					<DxButton
+						type="primary"
+						onClick={() => {
+							addSKU();
+						}}
+					>
+						Add Product
+					</DxButton>
+					<DxButton
+						type="primary"
+						onClick={() => {
+							setOnContactsPage(true);
+						}}
+						disabled={
+							(() => {
+								let allSaved = true;
+								console.log('a');
+								products
+									.filter((prod) => formsSavedStatus[prod.id] !== undefined)
+									.forEach((prod) => {
+										if (!formsSavedStatus[prod.id]) allSaved = false;
+									});
+								return !allSaved;
+							})() || products.length === 0
+						}
+					>
+						Finalize SKUs
+					</DxButton>
+				</div>
 			</div>
-			<div>{SKUList}</div>
-			<div className={`add-product ${addProductEnabled ? '' : 'hidden'}`}>
-				<DxButton
-					type="primary"
-					onClick={() => {
-						addSKU();
-					}}
-				>
-					Add Product
-				</DxButton>
-				<DxButton
-					type="primary"
-					onClick={() => {
-						console.log('xxxxx');
-					}}
-					disabled={
-						(() => {
-							let allSaved = true;
-							console.log('a');
-							products
-								.filter((prod) => formsSavedStatus[prod.id] !== undefined)
-								.forEach((prod) => {
-									if (!formsSavedStatus[prod.id]) allSaved = false;
-								});
-							return !allSaved;
-						})() || products.length === 0
-					}
-				>
-					Finalize SKUs
-				</DxButton>
-			</div>
+			<div className={`contacts-page ${onContactsPage ? '' : 'hidden'}`}>{getContactsForm()}</div>
 		</div>
 	);
 }
